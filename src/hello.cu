0025b33e9f2cc0f7dc64e1d55eb0ab2f720c1793
Olivier Kessler <olivier.kessler@protonmail.com>
2025-01-14 15:29:11 +0100

    Add some more examples

--- src/hello.cu
+++ src/hello.cu
@@ -1,18 +1,33 @@
 #include <stdio.h>
 
-__global__ void helloFromGpu(void)
+/**
+ * in_device_fn - This function call only be called from the device
+ */
+__device__ void in_device_fn()
 {
-    printf("Hello from GPU \n");
+    printf("Hello from in_device_fn() in GPU thread %d\n", threadIdx.x);
+}
+
+/**
+ * hello_from_gpu - This function can be called from device or host
+ */
+__global__ void hello_from_gpu(void)
+{
+    printf("Hello from GPU thread %d\n", threadIdx.x);
+    in_device_fn();
 }
 
 int main(void)
 {
     printf("Hello from CPU \n");
-    helloFromGpu <<<1, 10>>>();
+    hello_from_gpu <<<1, 10>>>();
+    printf("CPU waits \n");
+
     cudaError_t cudaerr = cudaDeviceSynchronize();
     if (cudaerr != cudaSuccess)
         printf("kernel launch failed with error \"%s\".\n",
                cudaGetErrorString(cudaerr));
+
     cudaDeviceReset();
     return 0;
 }
